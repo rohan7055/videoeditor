61551cee67ff69a805980cf92a0b32638eb0de50
rohan thakur <ron2prince@gmail.com>
2018-05-11 15:49:29 +0530

    fix error

--- app/src/main/java/admybrand/rohanthakur7055/com/videoeditor/MainActivity.java
+++ app/src/main/java/admybrand/rohanthakur7055/com/videoeditor/MainActivity.java
@@ -1,8 +1,15 @@
 package admybrand.rohanthakur7055.com.videoeditor;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaMetadataRetriever;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,12 +41,21 @@ public class MainActivity extends AppCompatActivity {
     Button capture;
     private final static int CAMERA_RQ = 6969;
     File saveFolder = new File(Environment.getExternalStorageDirectory(), "VideoEditingApp/Videos");
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=23) {
+
+            String[] PERMISSIONS = { Manifest.permission.READ_EXTERNAL_STORAGE,  Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            if(!hasPermissions(this, PERMISSIONS)){
+                ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_ASK_PERMISSIONS);
+            }
+        }
         capture = (Button)findViewById(R.id.capture);
         capture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +100,55 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this, data.getDataString().substring(7,data.getDataString().length()), Toast.LENGTH_SHORT).show();
             //Toast.makeText(this, saveFolder.getPath(), Toast.LENGTH_SHORT).show();
 
+        }
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS: {
+                if (grantResults.length == 2
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(i);
+                } else {
+                    AlertDialog.Builder builder;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        builder = new AlertDialog.Builder(MainActivity.this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar);
+                    } else {
+                        builder = new AlertDialog.Builder(MainActivity.this);
+                    }
+                    builder.setTitle("Please grant all the permissions")
+                            .setMessage("All these permissions are required for app to work seamlessly.\nChikoop will never steal any kind of your personal data.")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                                    finish();
+                                    startActivity(i);
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finishAffinity();
+                                }
+                            })
+                            .setIcon(R.mipmap.ic_launcher)
+                            .show();
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                }
+            }
         }
     }
 }
